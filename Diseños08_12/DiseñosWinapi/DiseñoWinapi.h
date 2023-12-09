24d946e491142eee501d016cc86e1c31ee8b8a5b
Isaias Barajas Garcia <116610561+Isaias1024@users.noreply.github.com>
2023-12-08 22:51:44 -0600

    Delete Diseños08_12 directory

--- Diseños08_12/DiseñosWinapi/DiseñoWinapi.h
+++
@@ -1,7 +0,0 @@
-#include "resource.h"
-
-LRESULT CALLBACK VentanaUsuario(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK VentanaRegistro(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK VentanaPrincipal(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-bool MenuUsuario(int opcion, HWND ventana);
-
